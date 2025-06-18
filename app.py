a3fd29bf92473a83925439f3848f619108bc661f
Sarawin Buakaew <sarawinbuakaewpp@gmail.com>
2025-06-18 22:56:41 +0700

    add dashboard page and Sidebar

--- app.py
+++ app.py
@@ -23,7 +23,21 @@ def home():
 
 
 @app.route('/login', methods=['GET', 'POST']) 
-def login(): 
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email') 
+        password = request.form.get('password')
+        
+        if email and password:
+            user = User.query.filter_by(email=email).first()
+            session['username'] = user.username
+            flash('Login successful!', 'success') 
+            return redirect('/dashboard')
+        else:
+            flash('Invalid email or password', 'danger')
+    else:
+        flash('Please enter both email and password', 'warning')
+        
     login_page = render_template('login.html')
 
     return login_page
@@ -32,9 +46,50 @@ def login():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
 
+    if request.method == 'POST':
+        username = request.form.get('username') 
+        email = request.form.get('email') 
+        password = request.form.get('password')
+        
+        if username and email and password:
+            existing_user = User.query.filter_by(email=email).first()
+            
+            if existing_user:
+                flash('Email already exits', 'danger')
+                
+                return redirect('/register')            
+            
+            new_usr = User(username=username,
+                           email=email,
+                           password=generate_password_hash(password))
+            db.session.add(new_usr) 
+            db.session.commit() 
+            flash('Registration successful!', 'success') 
+            
+            return redirect('/login') 
+
+        else:
+            flash('Please fill in all fields', 'warning') 
+            return redirect('/register') 
+        
     regis = render_template('register.html')
 
     return regis
+
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+def dashboard(): 
+    
+    return render_template('dashboard.html')
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/login')
+    
+    
+
 
 
 if __name__ == '__main__':
